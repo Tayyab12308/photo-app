f11a92b155178f67ac574bbb7d2d275614a5fc69
Tayyab Iqbal <tayyab12308@gmail.com>
2019-02-14 16:41:04 +0000

    production image upload issue

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,7 +7,6 @@ if Rails.env.production?
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
       region:               'us-east-2'
     }
-    config.fog_directory = ENV['S3_BUCKET']
-    config.fog_public = false
+    config.fog_directory = ENV['AWS_BUCKET']
   end
 end
